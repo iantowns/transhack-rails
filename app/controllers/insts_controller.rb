292d6f424c708390207422be29e39e468869272b
stickmoon <eh11@hampshire.edu>
2014-11-20 17:50:16 -0500

    inst model set up w/ CRUD functionality, shamelessly stolen from rails tutorial. currently only has first 2 fields b/c I was lazy. fixing later.

--- app/controllers/insts_controller.rb
+++ app/controllers/insts_controller.rb
@@ -1,12 +1,16 @@
 class InstsController < ApplicationController
   
   def new
+    @inst = Inst.new
   end
   
   def create
     @inst = Inst.new(inst_params)
-    @inst.save
-    redirect_to @inst
+    if @inst.save
+      redirect_to @inst
+    else
+      render 'new'
+    end
   end
   
   def show
@@ -16,6 +20,28 @@ class InstsController < ApplicationController
   def index
     @insts = Inst.all
   end
+  
+  def edit
+    @inst = Inst.find(params[:id])
+  end
+  
+  def update
+    @inst = Inst.find(params[:id])
+ 
+    if @inst.update(inst_params)
+      redirect_to @inst
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @inst = Inst.find(params[:id])
+    @inst.destroy
+ 
+    redirect_to inst_path
+  end
+
   
   private
     def inst_params
--- app/models/inst.rb
+++ app/models/inst.rb
@@ -1,3 +1,6 @@
 class Inst < ActiveRecord::Base
   has_many :orgs
+  validates :name, presence: true,
+                      length: { minimum: 3 }
+  
 end
